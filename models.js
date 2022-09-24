5448ad1ea0641f2ec59c4f14e9dbdbb1a436c912
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-09-24 18:45:05 +0100

    Modified models

--- models.js
+++ models.js
@@ -1,16 +1,40 @@
 import mongoose from "mongoose";
 
-const CommandSchema = new mongoose.Schema({
-  meterNo: {
-    type: String,
-    required: true,
-    minlength: 11,
-    maxlength: 11,
+const CommandSchema = new mongoose.Schema(
+  {
+    meterNumber: {
+      type: String,
+      required: true,
+      minlength: 11,
+      maxlength: 11,
+    },
+    cmd: {
+      type: String,
+      required: true,
+    },
+    SODNumber: {
+      type: String,
+    },
+    broadcastAddress: {
+      type: String,
+    },
+    meterResponse: {
+      type: [String],
+    },
   },
-  command: {
-    type: String,
-    required: true,
-  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+CommandSchema.virtual("command").get(function () {
+  let command = `?${this.cmd}=${this.meterNumber}`;
+  if (this.SODNumber) {
+    command += `,${this.SODNumber}!`;
+  } else if (this.broadcastAddress) {
+    command += `,${this.broadcastAddress}!`;
+  } else {
+    command += "!";
+  }
+  return command;
 });
 
 export const CommandModel = mongoose.model("Commands", CommandSchema);
